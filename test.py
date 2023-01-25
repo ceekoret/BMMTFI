fe47555e483ba1e06f5a81792c275b76bbd62b7e
Kees <keeeese@gmail.com>
2023-01-25 21:09:41 +0100

    TAQ code

--- test.py
+++ test.py
@@ -2,44 +2,51 @@ import os
 import pandas as pd
 import csv
 
-filedir = r"C:\Users\Ck0rt\Documents\Large files\School\MSc Finance & Investments\Thesis\Robintrack\popularity_export"
 
-bin_500 = 0
-bin_1000 = 0
-bin_10000 = 0
-bin_100000 = 0
-bin_plus = 0
+import wrds
 
-
-for filename in os.listdir(filedir):
-    csv_path = os.path.join(filedir, filename)
-
-    # checking if it is a file
-    if os.path.isfile(csv_path):
-        print(f"Now analysing data for Robintrack file: {filename}")
-        df = pd.read_csv(csv_path)
-
-        average_users = df.users_holding.mean().round()
-
-        if average_users <= 500:
-            bin_500 += 1
-        elif average_users <= 1000:
-            bin_1000 += 1
-        elif average_users <= 10000:
-            bin_10000 += 1
-        elif average_users <= 100000:
-            bin_100000 += 1
-        else:
-            bin_plus += 1
-
-
-print(f"Number of stocks in bin_500: {bin_500}")
-print(f"Number of stocks in bin_1000: {bin_1000}")
-print(f"Number of stocks in bin_10000: {bin_10000}")
-print(f"Number of stocks in bin_100000: {bin_100000}")
-print(f"Number of stocks in bin_plus: {bin_plus}")
-
-exit()
+conn = wrds.Connection()
+print(conn.list_libraries())
+company_narrow = conn.get_table(library='comp', table='company',
+                                columns = ['conm', 'gvkey', 'cik'], obs=5)
+# filedir = r"C:\Users\Ck0rt\Documents\Large files\School\MSc Finance & Investments\Thesis\Robintrack\popularity_export"
+#
+# bin_500 = 0
+# bin_1000 = 0
+# bin_10000 = 0
+# bin_100000 = 0
+# bin_plus = 0
+#
+#
+# for filename in os.listdir(filedir):
+#     csv_path = os.path.join(filedir, filename)
+#
+#     # checking if it is a file
+#     if os.path.isfile(csv_path):
+#         print(f"Now analysing data for Robintrack file: {filename}")
+#         df = pd.read_csv(csv_path)
+#
+#         average_users = df.users_holding.mean().round()
+#
+#         if average_users <= 500:
+#             bin_500 += 1
+#         elif average_users <= 1000:
+#             bin_1000 += 1
+#         elif average_users <= 10000:
+#             bin_10000 += 1
+#         elif average_users <= 100000:
+#             bin_100000 += 1
+#         else:
+#             bin_plus += 1
+#
+#
+# print(f"Number of stocks in bin_500: {bin_500}")
+# print(f"Number of stocks in bin_1000: {bin_1000}")
+# print(f"Number of stocks in bin_10000: {bin_10000}")
+# print(f"Number of stocks in bin_100000: {bin_100000}")
+# print(f"Number of stocks in bin_plus: {bin_plus}")
+#
+# exit()
 
 
 
