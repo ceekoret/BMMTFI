51db3d13ac8efb0a13c123d1fa9e8d4c4d4d87fe
Kees <keeeese@gmail.com>
2023-02-10 09:40:27 +0100

    reddit comments script

--- thesis/data_source/reddit/api_2.py
+++ thesis/data_source/reddit/api_2.py
@@ -15,8 +15,6 @@ from pathlib import Path
 import os.path
 
 
-# start = int(dt.datetime(2021,1,1,0,0).timestamp())
-# end = int(dt.datetime(2021,2,1,0,0).timestamp())
 
 # Directory where Reddit data is saved
 reddit_dir = r"C:/Users/Ck0rt/Documents/Large files/School/MSc Finance & Investments/Thesis/Reddit/posts"
@@ -27,6 +25,7 @@ end_date = dt.datetime(2020, 9, 1, 0, 0)
 
 subreddits = ['wallstreetbets', 'stocks', 'investing', 'stockmarket', 'pennystocks']
 
+save = False
 for subreddit in subreddits:
     # Create folder to save output
     folder_loc = os.path.join(reddit_dir, subreddit).replace('\\', '/')
@@ -45,7 +44,7 @@ for subreddit in subreddits:
         file_loc = os.path.join(folder_loc, year_and_month).replace('\\', '/') + ".csv"
 
         # Check if file already exists and skip API request if file exists
-        if os.path.isfile(file_loc):
+        if os.path.isfile(file_loc) and save:
             print(f"File exists: [{file_loc}]")
             date_time = date_time + relativedelta(months=1)
 
@@ -59,14 +58,20 @@ for subreddit in subreddits:
         start_time = time.time()
         posts = api.search_submissions(subreddit=subreddit, limit=300000, after=start, before=end)
         print(f'Retrieved {len(posts)} posts from Pushshift in [{time.time() - start_time}] seconds')
-
+        print(posts)
+        print(type(posts))
+        exit()
         # Save output to CSV via dataframe
         reddit_df = pd.DataFrame(posts)
 
         columns = ['author', 'created_utc', 'full_link', 'id', 'num_comments', 'score', 'selftext',
                    'subreddit', 'subreddit_id', 'subreddit_subscribers', 'title', 'url']
-        reddit_df.to_csv(file_loc, header=True, index=False, columns=columns)
-        print(f"Saving csv at [{file_loc}]")
+
+        if save:
+            reddit_df.to_csv(file_loc, header=True, index=False, columns=columns)
+            print(f"Saving csv at [{file_loc}]")
+        else:
+            print(reddit_df)
 
         # Adding 1 month to date_time tracker
         date_time = date_time + relativedelta(months=1)
